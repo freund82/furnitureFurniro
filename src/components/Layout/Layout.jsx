b75799343cf34c73fbf912e190de22c4e091712e
Maxim Utkin <maxim2001_2001@mail.ru>
2025-05-11 12:41:22 +0300

    Готовый layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "../Footer/Footer";
 
 export default function Layout({children}) {
   return (
-    <div className="layout">
+    <div className="layout"> {/*Шаблон сайта/страницы*/}
       <Header />
       <main>
         {children}
