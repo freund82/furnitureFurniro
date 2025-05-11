7ff091621e2e16b7b804a3e65db8877a2db5c2eb
Maxim Utkin <maxim2001_2001@mail.ru>
2025-05-11 19:07:17 +0300

    Старт страницы Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import Layout from '../components/Layout/Layout';
+import "./home.css"
 
 export default function Home() {
   return (
     <>
       <Layout>
-        
+        <section className="header-inner">
+
+        </section>
       </Layout>
     </>
   );
