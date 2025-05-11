28d86fbf96e2819de8f97866c3909a7442bacf67
Maxim Utkin <maxim2001_2001@mail.ru>
2025-05-11 21:30:02 +0300

    Готов блок Browse the Range

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,18 @@ import Layout from '../components/Layout/Layout';
 import "./home.css"
 
 export default function Home() {
+
+  const typesFurnitureImages=[
+    {
+      id:1, name:"Dining", image:"/furnitureImages/furnitureTypesImages/Dining.jpg",
+    },
+    {
+      id:2, name:"Living", image: "/furnitureImages/furnitureTypesImages/Living.jpg",
+    },
+    {
+      id:3, name:"Bedroom", image:"/furnitureImages/furnitureTypesImages/Bedroom.jpg",
+    }
+  ]
   return (
     <>
       <Layout>
@@ -12,6 +24,18 @@ export default function Home() {
             <p className="header-inner-block-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
             <button className="header-inner-block-button">Buy Now</button>
           </div>
+        </section>
+        <section className="furnitureTypes-block">
+              <h2 className="furnitureTypes-block-title">Browse the Range</h2>
+              <p className="furnitureTypes-block-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <div className="furnitureTypes-block-typesImages">
+                {typesFurnitureImages.map((item) => (
+                  <div key={item.id}>
+                     <img className="furnitureTypes-block-typesImages-image" src={item.image} alt={item.name}/>
+                     <p className="furnitureTypes-block-typesImage-name">{item.name}</p>
+                  </div>
+                ))}
+              </div>
         </section>
       </Layout>
     </>
