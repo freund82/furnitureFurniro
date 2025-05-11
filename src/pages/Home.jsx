f364597f35d8e255ead853275aa3fa1894ac78f8
Maxim Utkin <maxim2001_2001@mail.ru>
2025-05-11 19:39:05 +0300

    Готов header inner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,12 @@ export default function Home() {
     <>
       <Layout>
         <section className="header-inner">
-
+          <div className="header-inner-block">
+            <p className="header-inner-block-suptitle">New Arrival</p>
+            <h1 className="header-inner-block-title">Discover Our <br></br>New Collection</h1>
+            <p className="header-inner-block-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+            <button className="header-inner-block-button">Buy Now</button>
+          </div>
         </section>
       </Layout>
     </>
