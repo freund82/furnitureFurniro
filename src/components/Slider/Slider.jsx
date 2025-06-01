7bfd4119fe04d68da938fcf85a4357c73f423ac8
Maxim Utkin <maxim2001_2001@mail.ru>
2025-06-01 13:31:18 +0300

    Работающий слайдер но с ошибко отображения одинаковых горизонтальных фоток и без переключателей

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -6,6 +6,8 @@ import SliderArrow from "../../assets/icons/arrow.svg";
 export default function Slider(){
 
     const [currentSlide, setCurrentSlide] = useState(0);
+    const [currentImage, setCurrentImage] = useState(1);
+   
 
     const slides=[
         {
@@ -23,16 +25,32 @@ export default function Slider(){
          {
             id:2,
             number:"02",
-            title:"Luxury Room",
+            title:"Lux Room",
             category:"Bed Room",
             images:[
-                "/slider/bedroom_slider.jpg",
-                "/slider/bedroomOne.jpg",
-                "/slider/bedroomTwo.jpg",
-                "/slider/bedroomTwo.jpg",
+                "/slider/bedroom_slider2.jpg",
+                "/slider/livingRoomOne.jpg",
+                "/slider/livingRoomTwo.jpg",
+                "/slider/livingRoomThree.jpg",
             ]
         },
     ]
+
+    const nextSlide=()=>{
+        setCurrentSlide((prev)=>(prev === slides.length-1 ? 0 : prev+1));
+    }
+
+    const prevSlide=()=>{
+        setCurrentSlide((prev)=>(prev === 0 ? slides.length-1 : prev-1));
+    }
+
+    const nextImage=()=>{
+        setCurrentImage((prev)=>(prev === slides[currentSlide].images.length-1 ? 1 : prev+1));
+    }
+
+    const prevImage=()=>{
+        setCurrentImage((prev)=>(prev === 0 ? slides[currentSlide].images.length-1 : prev-1));
+    }
 
     return(
         <div className="slider-section-secondColumn">
@@ -42,12 +60,12 @@ export default function Slider(){
               <div className="sliderBlock">
                 <div className="sliderBlockHorizontal">
                     {
-                        slides[currentSlide].images.slice(1).map((img, index) =>
-                             <img key={index} className="slide" src={img} alt={`Slide ${index+1}`} />
+                        slides[currentSlide].images.slice(1).map((_, index) =>
+                             <img key={index} className="slide" src={slides[currentSlide].images[currentImage]} alt={`Slide ${index+1}`} />
                         )
                     }
-                 <span className="sliderHorizontal-button"><img src={SliderArrow} alt="SliderRightArrow" /></span>
-                 <span className="sliderHorizontal-button left hide"><img src={SliderArrow} alt="SliderRightArrow" /></span>
+                 <span className="sliderHorizontal-button" onClick={nextImage}><img src={SliderArrow} alt="SliderRightArrow" /></span>
+                 <span className="sliderHorizontal-button left hide" onClick={prevImage}><img src={SliderArrow} alt="SliderRightArrow" /></span>
                 </div>
                   <div className="sliderBlockHorizontal-buttons">
                     <span className="sliderBlock-button sliderButtonActive"></span><span className="sliderBlock-button"></span><span className="sliderBlock-button"></span><span className="sliderBlock-button"></span>
@@ -56,12 +74,12 @@ export default function Slider(){
                  <div className="sliderInfoBlock">
                     <div className="sliderInfoText">
                       <div className="sliderInfoText-numberSection">
-                        <span>01</span><span className="sliderInfoTetx-line"></span><span className="sliderInfoText-number">Bed Room</span>
+                        <span>{slides[currentSlide].number}</span><span className="sliderInfoTetx-line"></span><span className="sliderInfoText-number">{slides[currentSlide].category}</span>
                       </div>
-                      <h2 className="sliderInfoText-title">Inner Peace</h2>
+                      <h2 className="sliderInfoText-title">{slides[currentSlide].title}</h2>
                     </div>
                     <div className="sliderBlock-arrow">
-                      <img src={ArrowSmall} alt="SliderRightArrow" />
+                      <img src={ArrowSmall} alt="SliderRightArrow" onClick={nextSlide} />
                     </div>
                  </div>
             </div>
